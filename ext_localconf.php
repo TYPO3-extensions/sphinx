09887ea921abbef2aff62e5626f101a1f026cd3f
Xavier Perseguers <xavier@typo3.org>
2013-10-01 09:59:05 +0200

    [TASK] JSON files are editable
    It should be possible to edit typo3conf/sphinx-projects.json
    automatically when a mount point is available for typo3conf/.
    
    Change-Id: I41be59e0a181e1a2f74327cc7c633c1380cdf28a
    Reviewed-on: https://review.typo3.org/24196
    Reviewed-by: Xavier Perseguers
    Tested-by: Xavier Perseguers

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,16 +4,8 @@ if (!defined('TYPO3_MODE')) {
 }
 
 $textFileExtensions = explode(',', $GLOBALS['TYPO3_CONF_VARS']['SYS']['textfile_ext']);
-if (!in_array('rst', $textFileExtensions)) {
-	$textFileExtensions[] = 'rst';
-}
-if (!in_array('py', $textFileExtensions)) {
-	$textFileExtensions[] = 'py';
-}
-if (!in_array('yml', $textFileExtensions)) {
-	$textFileExtensions[] = 'yml';
-}
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['textfile_ext'] = implode(',', $textFileExtensions);
+$textFileExtensions = array_merge($textFileExtensions, array('json', 'py', 'rst', 'yml'));
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['textfile_ext'] = implode(',', array_unique($textFileExtensions));
 
 /** @var \TYPO3\CMS\Extbase\SignalSlot\Dispatcher $signalSlotDispatcher */
 $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher');
