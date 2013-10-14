f9f47c8f5795c8c4ce4f2b5471746a36a2789a5f
Xavier Perseguers <xavier@typo3.org>
2013-10-14 16:10:36 +0200

    [TASK] Check functionality with PHP 5.5 (thanks to Stefan Neufeind)
    Change-Id: I88c02318aa9d4ce86de7f134fe623c8ae6900b51
    Reviewed-on: https://review.typo3.org/24741
    Reviewed-by: Xavier Perseguers
    Tested-by: Xavier Perseguers

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.2.2-dev',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.3.3-5.4.99',
+			'php' => '5.3.3-5.5.99',
 			'typo3' => '6.0.0-6.2.99',
 		),
 		'conflicts' => array(
