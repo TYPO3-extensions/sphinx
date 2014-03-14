8757057c5f550746f7fbfc7f2ec307ab042220c9
Xavier Perseguers <xavier@typo3.org>
2014-03-14 19:42:40 +0100

    [TASK] Allow legacy Sphinx Console module to be hidden
    Change-Id: Iaf9e8767625527df4dd5cdf48692fcf175c6fa17
    Reviewed-on: https://review.typo3.org/28397
    Reviewed-by: Xavier Perseguers
    Tested-by: Xavier Perseguers

--- ext_tables.php
+++ ext_tables.php
@@ -4,8 +4,11 @@ if (!defined('TYPO3_MODE')) {
 }
 
 if (TYPO3_MODE === 'BE') {
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModulePath('file_txsphinxM1', \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/Controller/mod1');
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule('file', 'txsphinxM1', '', \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/Controller/mod1/');
+	$sphinxConfiguration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['sphinx']);
+	if (!isset($sphinxConfiguration['load_console_module']) || (bool)$sphinxConfiguration['load_console_module']) {
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModulePath('file_txsphinxM1', \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/Controller/mod1');
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule('file', 'txsphinxM1', '', \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/Controller/mod1/');
+	}
 
 	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
 		'Causal.' . $_EXTKEY,
