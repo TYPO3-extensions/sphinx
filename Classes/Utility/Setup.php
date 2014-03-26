8b1c3e055da58d1e4582e244d5a170bf8675971a
Xavier Perseguers <xavier@typo3.org>
2014-03-26 10:54:11 +0100

    [TASK] Proper sorting of Sphinx versions in EM
    Change-Id: I7e177166cd0e40c0313eaaf6750b2187a9ae6284
    Resolves: #57293
    Reviewed-on: https://review.typo3.org/28808
    Reviewed-by: Xavier Perseguers
    Tested-by: Xavier Perseguers

--- Classes/Utility/Setup.php
+++ Classes/Utility/Setup.php
@@ -1131,8 +1131,16 @@ EOT;
 			'#<tr class="iterable-item">.*?<td class="name">([^<]*)</td>.*?<a href="([^"]+)">zip</a>#s',
 			function($matches) use (&$versions) {
 				if ($matches[1] !== 'tip' && version_compare($matches[1], '1.1.3', '>=')) {
-					$versions[$matches[1]] = array(
-						'name' => $matches[1],
+					$name = $matches[1];
+					// Make sure main release (e.g., "1.2") gets a ".0" patch release version as well
+					if (preg_match('/^\d+\.\d+$/', $name)) {
+						$name .= '.0';
+					}
+					// Fix sorting of beta releases
+					$name = str_replace('b', ' beta ', $name);
+
+					$versions[$name] = array(
+						'name' => $name,
 						'url'  => $matches[2],
 					);
 				}
