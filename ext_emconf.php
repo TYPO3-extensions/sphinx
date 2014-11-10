9320f422fa1535305e6f5775bb43125340208b9d
Xavier Perseguers <xavier@typo3.org>
2014-11-10 14:34:43 +0100

    [TASK] Announce compatibility with TYPO3 7 LTS
    Change-Id: If406d2c7cb917c9ca82e5946eb6cc36f2c6ee34a
    Reviewed-on: http://review.typo3.org/33945
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Tested-by: Xavier Perseguers <xavier@typo3.org>

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,11 +29,11 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '2.1.1-dev',
+	'version' => '2.2.0-dev',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.3.3-5.5.99',
-			'typo3' => '6.0.0-6.2.99',
+			'php' => '5.3.3-5.6.99',
+			'typo3' => '6.0.0-7.99.99',
 		),
 		'conflicts' => array(
 		),
