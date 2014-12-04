ba0c3a420deb1aaef3cd6409402d98cc845e51d2
Xavier Perseguers <xavier@typo3.org>
2014-12-04 13:24:18 +0100

    [TASK] Upgrade CodeMirror to v.4.7
    Change-Id: I1783332064a4249cbf3e2d0a28c79b034388e3a6
    Reviewed-on: http://review.typo3.org/35033
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Tested-by: Xavier Perseguers <xavier@typo3.org>

---
+++ Resources/Public/JavaScript/create-codemirror.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SOURCE=~/Downloads/CodeMirror-4.7.0
+
+cp $SOURCE/lib/codemirror.js .
+cp $SOURCE/lib/codemirror.css ../Css/
+
+echo "/** mode/rst/rst.js */" >> codemirror.js
+cat $SOURCE/mode/rst/rst.js >> codemirror.js
+
+echo "/** mode/yaml/yaml.js */" >> codemirror.js
+cat $SOURCE/mode/yaml/yaml.js >> codemirror.js
+
+echo "/** addon/edit/trailingspace.js */" >> codemirror.js
+cat $SOURCE/addon/edit/trailingspace.js >> codemirror.js 
